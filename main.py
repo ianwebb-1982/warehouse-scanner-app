bbd37ecf7ac1c099a18f54a99096526dff696d4b
ianwebb-1982 <ianwebb1982@gmail.com>
2026-06-20 06:28:09 +0100

    added menu

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+from purchase_order import po_list
+from warehouse_location import wh_locations
+
+
+def main():
+   
+    print("WAREHOUSE MANAGEMENT APP")
+    print("--------------------")
+    print("Menu:")
+    print("1 - Purchase Orders")
+    print("2 - Store Locations")
+    print("--------------------")
+    print("Enter your selection: ")
+
+    user_menu_selection = input().lower()
+
+    
+    #for location in wh_locations:
+        #print(location)
+
+    #for po in po_list:
+        #print(po)
 
 
 
-def main()
-    pass    
-
-main
+main()
---
+++ purchase_order.py
@@ -0,0 +1,32 @@
+po_list = [12345,
+           12346,
+           12347,
+           12348,
+           12349,
+           12350,
+           12351,
+           12352,
+           12353,
+           12354,
+           12355,
+           12356,
+           12357,
+           12358,
+           12359,
+           12360,
+           12361,
+           12362,
+           12363,
+           12364,
+           12365]
+
+#to store PO's that are already in a location
+pos_in_location = []
+
+#store po's in tuple [(po, location)]
+
+# view po's
+    #view all po's
+    #view po's not in location
+    #view po's in locations
+
